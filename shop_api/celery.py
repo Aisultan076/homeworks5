52c2c4ccca6b55d05cc1e5b1c3aeb998d0a3a5e7
Aisultan <ajsultankockorbajuuluajsultan@gmail.com>
2025-08-26 23:44:55 +0600

    drf6_fix6_1

--- shop_api/celery.py
+++ shop_api/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop_api.settings")
 
-app = Celery("proj")
+app = Celery("shop_api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
@@ -13,6 +13,6 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     "print-hello-every-minute": {
         "task": "myapp.tasks.print_hello",
-        "schedule": crontab(minute="*"),  # каждую минуту
+        "schedule": crontab(minute="*"),
     },
 }
